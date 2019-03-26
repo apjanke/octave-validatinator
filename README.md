345247715c8de18f662aa83fb5c8aef719560e40
Andrew Janke <floss@apjanke.net>
2019-03-26 01:28:54 -0400

    Add initial validators

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# octave-validatentine
-Validator functions for GNU Octave
+Validatentine for GNU Octave
+============================
+
+Validation functions for GNU Octave.
+
+In Matlab R2017a, Matlab introduced [Validation Functions](https://www.mathworks.com/help/matlab/matlab_oop/property-validator-functions.html) that allow you concisely express requirements for the types and values of variable and property values.
+Octave doesn’t have those yet.
+This project provides implementations of them, both some Matlab-compatible ones, and some Octave-specific extensions.
---
+++ inst/extend/mustBeA.m
@@ -0,0 +1,42 @@
+## Copyright (C) 2019 Andrew Janke
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} mustBeA (x, type, label)
+##
+## Requires that input is of a given type.
+##
+## Raises an error if the input @code{x} is not of the specified @code{type}
+## or a subclass, as determined by @code{isa (x, type)}.
+##
+## @code{label} is an optional input that describes how the input will be described in
+## error messages. If not supplied, @code{inputname (1)} is used, and if that is
+## empty, it falls back to "input".
+##
+## @end deftypefn
+
+function mustBeA (x, type, label)
+  if nargin < 3; label = []; endif
+  if ! isa (x, type)
+    if isempty (label)
+      label = inputlabel (1);
+    endif
+    if isempty (label)
+      label = "input";
+    endif
+    error ("%s must be of type %s; got a %s", ...
+      label, type, class (x));
+  endif
+endfunction
